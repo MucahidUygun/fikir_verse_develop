5a5433cc2d6df73982fee034ff1e0a77e900d689
MucahidUygun <uygun3091@hotmail.com>
2023-01-11 01:36:11 +0300

    Firebase integration completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,47 @@
+import 'package:fikir_verse/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'page/splash/splash_view.dart';
 
-main() {
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+
+
+
+main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true
+  );
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) { 
+    print("Got a message foregasda");
+    print("mesaj:${message.data}");
+
+    if (message.notification !=null) {
+      print('Message also contained a notification: ${message.data}');
+    }
+
+  });
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
